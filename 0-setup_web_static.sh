4fac01388f746099f7a8f1713faddfb5c3eb1d69
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-04-05 01:07:58 +0100

    resolve requirement issues

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #set up a web server for the deployment of web_static
 
+source_dir="/data/web_static/releases/test/"
+symbolic_link="/data/web_static/current"
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/
@@ -9,10 +11,13 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-ln -sf  /data/web_static/releases/test/ 
-sudo chown -R "ubuntu":"ubuntu" /data/ /data/web_static/current
+if [ -L "$symbolic_link" ];
+then
+        rm -d "$symbolic_link"
+fi
+sudo ln -s "$source_dir" "$symbolic_link"
+sudo chown -R "ubuntu":"ubuntu" /data/
 
-sed -i "48i \\\tlocation /hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t\}" /etc/nginx/sites-available/default
+sudo sed -i "48i \\\tlocation /hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t\}" /etc/nginx/sites-available/default
 sudo nginx -t
 sudo service nginx restart
-
