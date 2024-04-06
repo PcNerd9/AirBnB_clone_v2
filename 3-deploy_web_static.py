4337820b7e89869085a7302945d39ba28753bbf4
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-04-06 18:01:38 +0100

    debugging 3-deploy_web_static.py v6

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,9 +3,10 @@
 contains fab function that creates and distributes an archive
 to my web servers
 """
-
+from importlib import import_module
 from fabric.api import run, local, env, put
-
+archive_module = import_module("1-pack_web_static")
+distribute_module = import_module("2-do_deploy_web_static")
 
 
 def deploy():
