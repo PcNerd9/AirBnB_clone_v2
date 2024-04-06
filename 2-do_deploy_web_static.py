b734b1f2b8286cb79fe6f13d59bb0e295a7b3d74
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-04-06 01:02:21 +0100

    wrote a script that distributes an archinve to your web server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a fabric file that distributes an archive to my web server
+"""
+
+from fabric.api import run, local, put, env
+from os.path import exists
+
+
+
+env.hosts = ['54.236.25.152', '100.26.17.86']
+def do_deploy(archive_path):
+    if exists(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1].split(".")[0]
+        location = "/data/web_static/releases/{}".format(filename)
+        put(archive_path, remote="/tmp/")
+        run("tar -xzvf archive_path -C {}".format(location))
+        run("rm -drf /tmp/{}.tgz".format(filename))
+        if (exists("/data/web_static/current")):
+            run("rm -drf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(location))
+        return True
+    except Exception as e:
+        return False
