08c204c52757403e8b78ee502a9174cd68ac47ee
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-04-06 14:47:47 +0100

    wrote a fab script that distributes an archive to my web server

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,11 +9,15 @@ from fabric.api import run, local, env
 archive_module = import_module("1-pack_web_static")
 distribute_module = import_module("2-do_deploy_web_static")
 
+archive_file = archive_module.do_pack()
+
+
 def deploy():
-    archive_file = archive_module.do_pack()
+    """
+    distrbutes an archive to my web server
+    uncompress it and move to the web folder
+    """
     if (not archive_file):
         return False
-    print("i and done with the first task")
     result = distribute_module.do_deploy(archive_file)
-    print("finished the second task")
     return (result)
