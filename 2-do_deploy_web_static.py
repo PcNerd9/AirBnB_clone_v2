69d46b3082dbe5384908091ebbacefad8d215561
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-04-06 14:24:51 +0100

    debugging 2-do_deploy_web_static.py script v5

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,14 +17,13 @@ def do_deploy(archive_path):
     try:
         filename = archive_path.split("/")[-1].split(".")[0]
         location = "/data/web_static/releases/{}/".format(filename)
-        put(archive_path, remote="/tmp/")
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(location))
         run("tar -xzvf /tmp/{}.tgz -C {}".format(filename, location))
         run("rm -drf /tmp/{}.tgz".format(filename))
         run("mv {}/web_static/* {}".format(location, location))
         run("rm -drf {}/web_static".format(location))
-        if (exists("/data/web_static/current")):
-            run("rm -drf /data/web_static/current")
+        run("rm -drf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(location))
         return True
     except Exception as e:
