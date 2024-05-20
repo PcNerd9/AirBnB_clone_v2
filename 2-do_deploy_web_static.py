ee325a48969864b27510169b00901768ebdde7ab
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-05-20 11:37:18 +0100

    debugging the 2-do_deploy_web_static.py file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,13 +20,13 @@ def do_deploy(archive_path):
         filename = archive_path.split("/")[-1].split(".")[0]
         location = "/data/web_static/releases/{}/".format(filename)
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(location))
-        run("sudo tar -xzvf /tmp/{}.tgz -C {}".format(filename, location))
+        run("mkdir -p {}".format(location))
+        run("tar -xzvf /tmp/{}.tgz -C {}".format(filename, location))
         run("rm -drf /tmp/{}.tgz".format(filename))
-        run("sudo mv {}/web_static/* {}".format(location, location))
-        run("sudo rm -drf {}/web_static".format(location))
-        run("sudo rm -drf /data/web_static/current")
-        run("sudo ln -sf {} /data/web_static/current".format(location))
+        run("mv {}/web_static/* {}".format(location, location))
+        run("rm -drf {}/web_static".format(location))
+        run("rm -drf /data/web_static/current")
+        run("ln -sf {} /data/web_static/current".format(location))
         return True
     except Exception as e:
         return False
