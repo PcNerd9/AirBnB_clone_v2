78a5b4797dcdb24a53ba1fdac458404bde358d3d
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-05-20 11:19:43 +0100

    resolve the bug in the 2-do_deploy_web_static.py file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,8 @@ a fabric file that distributes an archive to my web server
 
 from fabric.api import run, local, put, env
 from os.path import exists
-env.hosts = ['100.26.254.81', '100.26.158.71']
+env.hosts = ['100.26.254.81',
+             '100.26.158.71']
 env.user= "ubuntu"
 
 
@@ -19,13 +20,13 @@ def do_deploy(archive_path):
         filename = archive_path.split("/")[-1].split(".")[0]
         location = "/data/web_static/releases/{}/".format(filename)
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(location))
-        run("tar -xzvf /tmp/{}.tgz -C {}".format(filename, location))
+        run("sudo mkdir -p {}".format(location))
+        run("sudo tar -xzvf /tmp/{}.tgz -C {}".format(filename, location))
         run("rm -drf /tmp/{}.tgz".format(filename))
-        run("mv {}/web_static/* {}".format(location, location))
-        run("rm -drf {}/web_static".format(location))
-        run("rm -drf /data/web_static/current")
-        run("ln -sf {} /data/web_static/current".format(location))
+        run("sudo mv {}/web_static/* {}".format(location, location))
+        run("sudo rm -drf {}/web_static".format(location))
+        run("sudo rm -drf /data/web_static/current")
+        run("sudo ln -sf {} /data/web_static/current".format(location))
         return True
     except Exception as e:
         return False
