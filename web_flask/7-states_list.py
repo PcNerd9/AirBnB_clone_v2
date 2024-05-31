721cc2183f3bab49ff3cd4d85a7b858f9a3761f3
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-01 00:33:59 +0100

    wrote a script that start a flask application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(exec):
+    storage.handle_teardown(exec)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    all_states = storage.all()
+    return render_template("7-states_list.html", states=all_states.values())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
